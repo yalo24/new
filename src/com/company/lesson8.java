a8490e0dfcacea7d9af6c3f6260c78a2aef4e253
yalo24 <olgarizcovic@gmail.com>
2021-11-08 19:29:04 +0300

    new

---
+++ src/com/company/lesson8.java
@@ -0,0 +1,10 @@
+package com.company;
+
+
+import java.util.Arrays;
+import java.util.Random;
+public class lesson8 {
+    public static void main(String[] args) {
+        System.out.println("привет мир!!!");
+    }
+}
