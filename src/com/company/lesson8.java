14269dee269e061badcc528aa1c586f3cb479998
yalo24 <olgarizcovic@gmail.com>
2021-11-08 19:48:33 +0300

    else

--- src/com/company/lesson8.java
+++ src/com/company/lesson8.java
@@ -6,5 +6,6 @@ import java.util.Random;
 public class lesson8 {
     public static void main(String[] args) {
         System.out.println("привет мир!!!");
+        System.out.println("клон");
     }
 }
