90885ff2c3240d5cca0dd4cbdca196a99de49f75
yalo24 <olgarizcovic@gmail.com>
2021-11-08 21:44:45 +0300

    met

--- src/com/company/lesson8.java
+++ src/com/company/lesson8.java
@@ -1,12 +1,45 @@
 package com.company;
 
 
-import java.util.Arrays;
-import java.util.Random;
 public class lesson8 {
     public static void main(String[] args) {
-        System.out.println("привет мир!!!");
-        System.out.println("клон");
-        System.out.println("клон");
+
+  /*    int c= dlt (49, 7);
+        System.out.println(c);
     }
+
+    public static int dlt (int a, int b){
+        System.out.println(a/b);
+        return a / b;
+    }
+        dlt ();
+    }
+
+    public static void dlt() {
+        System.out.println(800);
+    }*/
+               System.out.println(sum(one(), two(), three()));
+    }
+
+    public static int sum (int ...a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+
+        }
+        return sum;
+    }
+        public static int one (){
+
+        return 1;
+    }
+        public static int two (){
+
+            return 2;
+        }
+        public static int  three(){
+
+            return 3;
+        }
 }
+
